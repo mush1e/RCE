a82504484cc9f0d0c51e411c67151b42a19b92ec
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2024-05-14 16:42:09 -0400

    bug/ file data not being transferred maybe have a new form and new api to handle it?

--- src/controller.cpp
+++ src/controller.cpp
@@ -385,7 +385,7 @@ void handle_add_problem(HTTPRequest& req, int client_socket) {
     std::string http_response {};
     std::string title = get_form_field(req.body, "question_title");
     std::string text = get_form_field(req.body, "question_text");
-    std::cout << title << "I'm here "<< std::endl;
+    std::cout << title << ", " << text << std::endl;
 
     title = escape_string(title);
     text = escape_string(text);
